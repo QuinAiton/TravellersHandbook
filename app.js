0bec6df3e018bf272c6ce8961856031d373eed5d
Quinten Aiton <QuinAiton@hotmail.com>
2020-08-10 09:39:00 -0700

    changed port function for heroku

--- app.js
+++ app.js
@@ -82,6 +82,7 @@ app.use("/campgrounds", CampgroundRoutes);
 app.use("/campgrounds/:id/comments", CommentRoutes);
 app.use(indexRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(" YelpCamp server launching on port 3000!...");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(" YelpCamp server has Started");
 });
